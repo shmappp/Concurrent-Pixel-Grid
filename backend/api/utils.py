276ad9d6dec52c50663323ee5b31c06fe8dd1558
Shams <shamssaaticho9@gmail.com>
2025-05-28 02:50:58 +1000

    fix: changed to 1D array to match frontend canvas format

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -2,12 +2,15 @@ from .models import Pixel
 import os
 
 def load_canvas_state(n, m):
-    canvas = [[{'color': '#FFFFFF', 'user': None} for _ in range(n)] for _ in range(m)]
+    canvas = []
+    for j in range(m):
+        for i in range(n):
+            canvas.append({'x':i, 'y':j, 'color': '#FFFFFF', 'user': None})
 
     for pixel in Pixel.objects.all():
-        canvas[pixel.y][pixel.x] = {pixel.color, pixel.user}
+        canvas[pixel.y*n + pixel.x] = {'x':pixel.x, 'y':pixel.y, 'color':pixel.color, 'user':pixel.user}
     
     return canvas
 
 def get_canvas_size():
-    return (os.environ.get('CANVAS_ROWS', 20), os.environ.get('CANVAS_COLS', 20))
+    return (os.environ.get('CANVAS_ROWS', 50), os.environ.get('CANVAS_COLS', 50))
