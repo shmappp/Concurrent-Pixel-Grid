98233669428a12910496f56ba62fb59c16156c53
Shams <shamssaaticho9@gmail.com>
2025-05-28 01:31:13 +1000

    added placeholder default pixel drawing logic

--- frontend/src/components/Canvas.tsx
+++ frontend/src/components/Canvas.tsx
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { type Pixel } from "../types"
 import { Square } from './Square'
+import { useCanvasSocket } from '../hooks/useCanvasSocket';
 
 const rows = 50
 const cols = 50
 
-export const Canvas = () => {
-    const pixels: Pixel[] = [];
-    for (let row = 0; row < rows; row++) {
-        for (let col = 0; col < cols; col++) {
-            pixels.push({x:col, y:row, color:'#3495eb', user:'test'})
+export const Canvas = (user: string) => {
+    const [pixels, setPixels] = useState<Pixel[]>(Array(rows*cols).fill(null));
+    const { sendPixelUpdate, lastMessage, isConnected } = useCanvasSocket(user);
+
+    useEffect(() => {
+        let defaultPixels: Pixel[] = []
+        for (let row = 0; row < rows; row++) {
+            for (let col = 0; col < cols; col++) {
+                defaultPixels.push({x:col, y:row, color:'#FFFFFF', user:null})
+            }
         }
-    }
+        setPixels(defaultPixels);
+    }, [])
+
     return (
         <div 
-            style={{ 
+            style={{
                 justifyContent: 'center',
                 display: 'grid',
                 gridTemplateColumns: `repeat(${cols}, 20px)`,
@@ -22,7 +30,7 @@ export const Canvas = () => {
                 gap:'1px'
             }}>
             {pixels.map((pixel, idx) => (
-                <Square color={pixel.color} />
+                pixel && <Square color={pixel.color} />
             ))}
         </div>
     )
