ae976426d10ed066d1fe54acc2d7709ac3c48fc7
Shams <shamssaaticho9@gmail.com>
2025-05-27 19:21:50 +1000

    feat: added websocket dependencies

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,13 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api'
 ]
+
+ASGI_APPLICATION = 'backend.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
