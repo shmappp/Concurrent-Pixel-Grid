739466a800935d380c5277c8899a5f410af171dc
Shams <shamssaaticho9@gmail.com>
2025-05-28 01:25:20 +1000

    feat: added canvas websocket wrapper

---
+++ frontend/src/hooks/useCanvasSocket.tsx
@@ -0,0 +1,25 @@
+import useWebSocket, { ReadyState } from 'react-use-websocket'
+import { type Pixel } from '../types'
+
+
+export const useCanvasSocket = (user: string) => {
+    const socketUrl = `ws://localhost:8000/ws/socket-server/` // TODO: change to production link 
+
+    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
+        socketUrl,
+        {
+            share: false,
+            shouldReconnect: () => true
+        }
+    )
+
+    return {
+        sendPixelUpdate: (pixel: Pixel) => {
+            sendJsonMessage({ type: 'place_pixel', x:pixel.x, y:pixel.y, color:pixel.color, user:user });
+        },
+        lastMessage: lastJsonMessage,
+        readyState,
+        isConnected: readyState === ReadyState.OPEN
+    }
+    
+}
