ca6a1f2e0a27c94345037c2843ba415fa7deb33a
Shams <shamssaaticho9@gmail.com>
2025-05-27 18:09:18 +1000

    feat: added Pixel model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Pixel(models.model):
+    x = models.PositiveIntegerField()
+    y = models.PositiveIntegerField()
+    color = models.CharField(max_length=7) 
+    user = models.CharField(max_length=64)
+
+    class Meta:
+        unique_together = ('x', 'y')
+    
