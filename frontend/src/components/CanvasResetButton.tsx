484b69274cef07053f44c61a5439464209c3955f
Shams <shamssaaticho9@gmail.com>
2025-05-28 18:01:57 +1000

    changed styling

--- frontend/src/components/CanvasResetButton.tsx
+++ frontend/src/components/CanvasResetButton.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button'
 
+
 interface CanvasResetButtonProps {
     onClick: () => void;
 }
 
 export const CanvasResetButton: React.FC<CanvasResetButtonProps> = ({ onClick }) => {
     return (
-        <Button variant='Info' onClick={onClick} >Reset Canvas</Button>
+        <Button variant='warning' onClick={onClick} >Reset Canvas</Button>
     )
 }
