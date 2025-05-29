31f9d66d01f32cf39ff4bb710ff81ae9f7452dd3
Shams <shamssaaticho9@gmail.com>
2025-05-29 19:45:24 +1000

    feat: copy style of Login to Home, add return to login button

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,12 +5,14 @@ import '../css/Home.css'
 import CanvasColorPicker from '../components/CanvasColorPicker';
 import { useCanvasContext } from '../context/CanvasContext';
 import Button from 'react-bootstrap/Button'
+import { useNavigate } from 'react-router-dom';
 // import { HexColorPicker } from "react-colorful";
 
 export const Home = () => {
     const [resetTrigger, setResetTrigger] = useState(0);
     const { user, setUser, selectedColor, setSelectedColor } = useCanvasContext();
     const [localUser, setLocalUser] = useState(null)
+    const navigate = useNavigate();
 
     const handleCanvasResetClick = useCallback(() => {
         setResetTrigger(prev => prev + 1)
@@ -21,9 +23,13 @@ export const Home = () => {
     }
 
     return (
+
         <div className='Home'>
             <title>Concurrent Pixel Grid</title>
             <div style= {{ display:'flex', justifyContent:'center', margin:'20px' }}>
+                <div style= {{ display:'flex' }}>
+                    <button className='return-button' onClick={() => navigate('/login', { viewTransition:true })}>Return to login</button>
+                </div> 
                 <CanvasResetButton onClick={handleCanvasResetClick} />
             </div>
             <div className='center-container'>
@@ -34,12 +40,13 @@ export const Home = () => {
                     <CanvasColorPicker color={selectedColor} setColor={setSelectedColor} />
                 </div>
             </div>
-            <div style= {{ display:'flex', justifyContent:'center' }}>
+            {/* <div style= {{ display:'flex', justifyContent:'center' }}>
                 <input placeholder='type username' onChange={(e) => setLocalUser(e.target.value)}></input>
                 <Button variant='outline-info' onClick={handleUserChange}>Apply</Button>
-            </div>
+            </div> */}
             <div style= {{ display:'flex', justifyContent:'center' }}>
-                <font>Current user: {user}</font>
+                <div style={{ 'font-family':'ZCOOL QingKe HuangYou', 'font-size': '20px' }}>Current user - {user}</div>
+                
             </div>
         </div>
     )
